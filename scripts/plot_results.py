35744fe58f7681c258d9744a320b999b771d3cfe
hyunrok <hyunrok90@gmail.com>
2021-01-02 21:02:45 -0500

    Added visualization for per step transition error

--- scripts/plot_results.py
+++ scripts/plot_results.py
@@ -3,10 +3,8 @@ File containing plotting mechanisms for different visualizations
 """
 
 import gym
-import safety_gym
 import numpy as np
 import os
-from scripts.utils import MujocoNormalized
 import argparse
 from rl_gat.reinforcedgat import GroundedEnv
 import matplotlib.pyplot as plt
@@ -14,19 +12,21 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
 import warnings
 warnings.filterwarnings("ignore")
 from stable_baselines import TRPO, PPO2
+import seaborn as sns
   
 def calculate_transition_errors(sim_env, real_env, policy, action_tf_policy_list):
     
     deterministic = True
     
     transition_errors = []
-    num_episodes = 5
+    num_episodes = 50
     j = 0
     for action_tf_policy in action_tf_policy_list:
         print(' Begin Iteration ',j)
         l2_error_list = []
         for i in range(num_episodes):
            
+        
             grounded_env = GroundedEnv(sim_env,
                                    action_tf_policy= action_tf_policy,
                                    debug_mode=False,
@@ -35,22 +35,36 @@ def calculate_transition_errors(sim_env, real_env, policy, action_tf_policy_list
             
             done = False
             obs = real_env.reset()
-            grounded_env.reset_state(obs)
-            
+            grounded_env.reset()
+
             while not done:
+                grounded_env.reset_state(obs)
                 action, _state = policy.predict(obs, deterministic=deterministic)
                 obs, _, done, _ = real_env.step(action)
-                grounded_env.reset_state(obs)
                 grounded_obs, _, done_grounded, _ = grounded_env.step(action)
                 l2_error_list.append(np.linalg.norm(grounded_obs - obs))
                 if done_grounded:
-                    print('Broke at target')
-            
+                    print('Broke at grounded environment')
+        
         transition_errors.append(np.mean(l2_error_list))
         print('Transition error at this iteration', np.mean(l2_error_list))
         j = j+1
     
     return transition_errors
+
+def plot_results(results_dict):
+    
+    window = 5
+    
+    for key, result in results_dict.items():
+        running_average = np.convolve(result, np.ones(window)*1/window, mode="valid")
+        x = np.arange(window, running_average.shape[0]+window)
+        sns.lineplot(x, running_average, label=key)
+
+    plt.title('Transition Error between Target and Grounded Environments')
+    plt.xlabel('Action Transformation Policy updates')
+    plt.ylabel('Average per-step transition error')
+    plt.show()
 
 def main():
     
@@ -70,21 +84,37 @@ def main():
     
     policy = TRPO.load(args.load_policy_path)
     
-    action_tf_policy_list = []
+    action_tf_policy_list_single = []
+    action_tf_policy_list_double = []
     
     num_grounding = 50
     
+    atp_path_single = args.load_atp_path
+    atp_path_double = args.load_atp_path.replace('_0','_1')
     print('################## Begin File loading ##################')
     for index in range(num_grounding):
-        file_path = os.path.join(args.load_atp_path,"action_transformer_policy1_"+str(index)+".pkl")
-        print(file_path)
-        action_tf_policy_list.append(PPO2.load(file_path))
+        file_path_single = os.path.join(atp_path_single,"action_transformer_policy1_"+str(index)+".pkl")
+        print(file_path_single)
+        action_tf_policy_list_single.append(PPO2.load(file_path_single))
+        file_path_double = os.path.join(atp_path_double,"action_transformer_policy1_"+str(index)+".pkl")
+        print(file_path_double)
+        action_tf_policy_list_double.append(PPO2.load(file_path_double))
             
-    
+    results_dict = {}
     print('################## File loading Completed ##################')
     
-    results = calculate_transition_errors(sim_env, real_env, policy, action_tf_policy_list)
-
+    results_single = calculate_transition_errors(sim_env, real_env, policy, action_tf_policy_list_single)
+    
+    print('############## Begin Double Discriminator Calculations')    
+    
+    results_double = calculate_transition_errors(sim_env, real_env, policy, action_tf_policy_list_double)
+    
+    results_dict['GARAT'] = results_single
+    results_dict['GARAT Double Discriminator'] = results_double
+    
+    plot_results(results_dict)
+    
+    
 if __name__ == '__main__':
     main()
     os._exit(0)
